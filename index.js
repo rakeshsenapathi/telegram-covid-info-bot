d19bd637922f4aaeb07f9e4ca2403398ce3807ea
rakeshsenapathi <rakeshsenapathi@gmail.com>
2020-04-08 03:55:55 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -3,33 +3,29 @@
 // Import dependencies, Setup http server, setup telegram-bot api
 require('dotenv').config();
 const express = require('express'),
-    bodyParser = require('body-parser'),
+    { urlencoded, json } = require('body-parser'),
     TelegramBot = require('node-telegram-bot-api'),
     app = express(),
     bot = new TelegramBot(process.env.TELEGRAM_ACCESS_TOKEN, { polling: true });
 
 // Parse application/x-www-form-urlencoded
 app.use(
-    bodyParser.urlencoded({
+    urlencoded({
         extended: true
     })
 );
-app.use(bodyParser.json());
+app.use(json);
 
-bot.on("text", function (message) {
+bot.on("text", (message) => {
     bot.sendMessage(message.chat.id, "Hello World");
 });
 
 // Set the listening port.
 app.set('PORT', process.env.PORT || 5000);
 
-app.get('/', function (req, res) {
-    res.send("Home")
-});
+app.get('/', (req, res) => res.send("Home"));
 
 // Listen for requests
-app.listen(app.get('PORT'), function () {
-    console.log("Your app is listening port : " + app.get('PORT'))
-});
+app.listen(app.get('PORT'), () => console.log("Your app is listening port : " + app.get('PORT')));
 
 
